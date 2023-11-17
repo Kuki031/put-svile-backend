9b59f09a0481c2e9cb0ad9be0656853b9ad01fa6
Kuki031 <luka.kukucka1@gmail.com>
2023-11-17 20:17:39 +0100

    Add filtering

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -5,13 +5,21 @@ const apiError = require('../utils/apiError');
 
 exports.getAllFacts = async (req, res, next) => {
     try {
-        const facts = await Fact.find({}, { _id: 0 });
+        const objQuery = { ...req.query };
+        const filterFields = ["sort", "paginate", "project", "field"];
+        filterFields.forEach(field => delete objQuery[field]);
+        let queryString = JSON.stringify(objQuery);
+        queryString = queryString.replace(/(gt|gte|lt|lte)/g, (el) => `$${el}`);
+
+        let query = JSON.parse(queryString);
+        const facts = await Fact.find(query, { _id: 0 });
         res.status(200).json({
             status: 'success',
             facts
         })
     }
     catch (err) {
+        console.log(err);
         return next(new apiError('Something went wrong.', 500));
     }
 }
@@ -35,7 +43,10 @@ exports.getOneFact = async (req, res, next) => {
 
 exports.createNewFact = async (req, res, next) => {
     try {
-        const newFact = await Fact.create(req.body);
+        const newFact = await Fact.create({
+            factKey: req.body.factKey,
+            lang: req.body.lang
+        });
         res.status(201).json({
             status: 'success',
             newFact
