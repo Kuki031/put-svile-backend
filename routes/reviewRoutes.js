fdeb9a5b9f7995badc6928c8adbaa6bfc7f7112b
Kuki031 <luka.kukucka1@gmail.com>
2023-11-19 00:03:01 +0100

    Remove rate limit

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,20 +1,13 @@
 'use strict';
 
 const express = require('express');
-const rateLimit = require('express-rate-limit');
 const reviewController = require('../controllers/reviewController');
 const reviewRouter = express.Router();
 
-const limiter = rateLimit({
-    limit: 1,
-    windowMs: 7 * 24 * 60 * 1000,
-    message: "Već ste recenzirali ovu stranicu!",
-    validate: { trustProxy: false }
-})
 
 reviewRouter.route('/').get(reviewController.getAllReviews);
 reviewRouter.route('/rating').get(reviewController.reviewStatistics);
 
-reviewRouter.use(limiter);
+
 reviewRouter.post('/', reviewController.createReview)
 module.exports = reviewRouter;
