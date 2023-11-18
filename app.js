7ca16fe6e9ce2e065368140cfed7d1324dc88396
Kuki031 <luka.kukucka1@gmail.com>
2023-11-18 15:27:03 +0100

    Implement compression and fix cors

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const sanitizer = require('perfect-express-sanitizer');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const app = express();
 const quoteRouter = require('./routes/quoteRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -15,16 +17,19 @@ const errorController = require('./controllers/errorController');
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }))
-app.use(cors({
-    allowedHeaders: '*',
-    origin: '*'
-}));
+app.use(cors());
+app.options('*', cors());
 app.use(sanitizer.clean({
     xss: true,
     noSql: true,
     sql: true
 }))
-
+app.use(compression());
+app.use(cookieParser());
+app.use(express.urlencoded({
+    extended: true,
+    limit: '10kb'
+}))
 app.use('/api/v1/quotes', quoteRouter);
 app.use('/api/v1/reviews', reviewRouter);
 
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,19 +3,24 @@
 const Review = require('../models/reviewModel');
 const apiError = require('../utils/apiError');
 const apiFeatures = require('../utils/apiFeatures');
-
+const cookieOptions = {
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true
+}
 exports.createReview = async (req, res, next) => {
     try {
         const newReview = await Review.create({
             rating: req.body.rating,
             comment: req.body.comment
         });
-        res.status(201).json({
+        if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+        res.status(201).cookie('recenzija', '1', cookieOptions).json({
             status: 'success',
             newReview
         })
     }
     catch (err) {
+        console.log(err);
         return next(new apiError('Something went wrong.', 500));
     }
 }
